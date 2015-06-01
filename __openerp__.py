230658b89c31cc1afa01d4061b5c259bc0aadec6
Ben Swanson <benswanson@rakocontrols.com>
2015-06-01 12:12:10 +0100

    Initial Commit

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Partner Discount Field',
+    'version': '1.0',
+    'category': '',
+    "sequence": 14,
+    'complexity': "easy",
+    'category': 'Hidden',
+    'description': """
+        Adds discount field to partners
+    """,
+    'author': 'Bentech',
+    'website': 'https://github.com/bentech',
+    'depends': [],
+    'init_xml': [],
+    'data': ['partner_view.xml'],
+    'demo_xml': [],
+    'test': [],
+    'qweb' : [],
+    'installable': True,
+    'auto_install': True,
+}
